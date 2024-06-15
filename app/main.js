76748bec8b71da2a007eb97972777706ba0d15c1
Isabela Nunes <isabelanunes533905@gmail.com>
2024-06-14 22:21:43 -0300

    fazendo forEach

--- app/main.js
+++ app/main.js
@@ -1,8 +1,32 @@
-let livros = enpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
-getBuscarLivrosDaAPI()
+let livros = [];
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+const elementoParaInserirLivros = document.getElementById('livros');
 
 async function getBuscarLivrosDaAPI() {
-    const res = await fetch(endpointDaAPI)
-    livros = await res.json()
-    console.log(livros)
+    const res = await fetch(endpointDaAPI);
+    livros = await res.json();
+    console.log(livros);
+    exibirOsLivrosNaTela(livros);
 }
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML +=
+        `<div class="livro">
+      <img class="livro__imagens " src="${livro.imagem}"
+        alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+      ${livro.titulo}
+        Cangaceiro JavaScript +:
+        O Retorno do cangaceiro JavaScript
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${livro.preco}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>`;
+    });
+}
+
+getBuscarLivrosDaAPI();
